011341e2a452472184738a48879c0c20079571a8
adiuniverse <uadi95060@gmail.com>
2022-08-23 10:06:22 +0530

    change the code

--- customer/views.py
+++ customer/views.py
@@ -5,6 +5,8 @@ def customer_home(request):
     return render(request,'customer/home.html')
 def customer_about(request):
     return render(request,'customer/about.html')
+def customer_cart(request):
+    return render(request,'customer/cart.html')
 
 
 
