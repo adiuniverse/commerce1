fe9af6663ca987a5ad8925fcb3f21200366c9e09
adiuniverse <uadi95060@gmail.com>
2022-08-23 09:18:58 +0530

    base path set

--- commerce1/urls.py
+++ commerce1/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('customer/',include('customer.urls'))
+     path('',include('customer.urls'))
 ]
 
 
--- customer/urls.py
+++ customer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home' ,views.customer_home,name='picture'),
+    path('' ,views.customer_home,name='picture'),
    path('about' ,views.customer_about,name='image')
    
 ]
